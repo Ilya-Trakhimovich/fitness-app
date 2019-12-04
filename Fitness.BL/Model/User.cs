ad713903002e00e737053b6cf6b168fecaecc5ea
Ilya_Trakhimovich <Ilya.Naroch@gmail.com>
2019-12-04 19:27:43 +0300

    Added

--- Fitness.BL/Model/User.cs
+++ Fitness.BL/Model/User.cs
@@ -2,9 +2,10 @@
 
 
 namespace Fitness.BL.Model
-{/// <summary>
-/// Пользователь
-/// </summary>
+{   /// <summary>
+    /// Пользователь
+    /// </summary>
+    [Serializable]
     public class User
     {
         #region Свойства
@@ -15,11 +16,11 @@ namespace Fitness.BL.Model
         /// <summary>
         /// Пол
         /// </summary>
-        public Gender Gender { get; }
+        public Gender Gender { get; set; }
         /// <summary>
         /// Дата рождения
         /// </summary>
-        public DateTime BirthDate { get; }
+        public DateTime BirthDate { get; set; }
         /// <summary>
         /// Вес
         /// </summary>
@@ -28,6 +29,9 @@ namespace Fitness.BL.Model
         /// Рост
         /// </summary>
         public double Height { get; set; }
+
+        public int Age { get { return DateTime.Now.Year - BirthDate.Year; } }
+
         #endregion
 
         /// <summary>
@@ -78,9 +82,18 @@ namespace Fitness.BL.Model
             Height = height;
         }
 
+        public User(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым.", nameof(name));
+            }
+            Name = name;
+        }
+
         public override string ToString()
         {
-            return Name;
+            return $"{Name} - {Age}";
         }
     }
 }
--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Fitness.BL.Controller;
+using Fitness.BL.Model;
+using System;
 
 namespace Fitness.CMD
 {
@@ -6,7 +8,65 @@ namespace Fitness.CMD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Вас приветствует приложение Fitness_1.");
+
+            Console.WriteLine("Введите имя пользовтеля.");
+            var name = Console.ReadLine();
+
+            var userController = new UserController(name);
+
+            if (userController.IsNewUser)
+            {
+                Console.Write("Введите пол:");
+
+                var gender = Console.ReadLine();
+
+                var birthDate = ParseDateTime();
+                var weight = ParseDouble("вес");
+                var height = ParseDouble("рост");
+
+                userController.SetNewUserData(gender, birthDate, weight, height);
+            }
+
+            Console.WriteLine(userController.CurrentUser);
+            Console.ReadLine();
+        }
+
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.Write("Введите дату рождения (dd.MM.yyyy):");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты рождения");
+                }
+            }
+
+            return birthDate;
+        }
+
+        private static double ParseDouble(string name)
+        {
+            while (true)
+            {
+                Console.Write($"Введите {name}: ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный формат {name}а");
+                }
+            }
         }
     }
+
+    
 }
